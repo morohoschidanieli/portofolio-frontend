c2be8232db7f6b177afe347118c1074bcb487530
personal <moro.daniel@yahoo.com>
2023-07-28 16:20:19 +0300

    ADDED PORTFOLIO PROJECT

--- src/Components/route/Portofolio.js
+++ src/Components/route/Portofolio.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import H2 from "../Headings/H2";
 import PortofolioLayout from "../Layouts/PortofolioLayout";
-import ResumeMockup from "../../assets/images/projects/resume-mockup.png";
 import BlackjackMockup from "../../assets/images/projects/brackjack-mockup.png";
 import CalculatorMockup from "../../assets/images/projects/calculator-mockup.png";
 import GuessMyNumberMockup from "../../assets/images/projects/guessmynumber-mockup.png";
@@ -9,6 +8,8 @@ import RandomNumberGenerator from "../../assets/images/projects/randomnumbergene
 import TicTacToeMockup from "../../assets/images/projects/tictactoe-mockup.png";
 import ToDoMockup from "../../assets/images/projects/todo-mockup.png";
 import LamaSocialMockup from "../../assets/images/projects/Lamasocial-mockup.png";
+import HomeAutomationSystemMockup from "../../assets/images/projects/smart-home-automation-system-mockup.png";
+import PortofolioMockup from "../../assets/images/projects/portofolio-mockup.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCompass } from "@fortawesome/free-solid-svg-icons";
 
@@ -60,16 +61,22 @@ const LiveDemoLink = styled.a(
 const Portofolio = () => {
   const projects = [
     {
+      title: "Portofolio",
+      image: PortofolioMockup,
+      url: "https://github.com/morohoschidanieli/portofolio-frontend",
+      typeOfUrl: "Github",
+    },
+    {
       title: "Lama social",
       image: LamaSocialMockup,
       url: "https://github.com/morohoschidanieli/lama-social",
       typeOfUrl: "Github",
     },
     {
-      title: "Resume",
-      image: ResumeMockup,
-      url: "https://morohoschidanieli.github.io/portofolio-html-boostrap/",
-      typeOfUrl: "Live Demo",
+      title: "Smart home automation system",
+      image: HomeAutomationSystemMockup,
+      url: "https://github.com/morohoschidanieli/raspberry-pi4-thesis",
+      typeOfUrl: "Github",
     },
     {
       title: "Blackjack Game",
