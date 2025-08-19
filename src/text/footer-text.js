054daa8bd11089a887657efbfc2bdf2f791f3d8c
Daniel-Iosif Morohoschi <daniel-iosifmorohoschi@local.local>
2025-08-19 17:26:04 +0300

    chore: remove previous code

--- src/text/footer-text.js
+++
@@ -1,9 +0,0 @@
-const date = new Date();
-const currentYear = date.getFullYear();
-
-const footerText = {
-  createdBy: "created with ♥ by Daniel-Iosif Morohoschi",
-  currentYear: `© ${currentYear}`,
-};
-
-export default footerText;
--- src/text/home-text.js
+++
@@ -1,6 +0,0 @@
-const homeText = {
-  h1: "created with ♥ by Daniel-Iosif Morohoschi",
-  h2: "© 2022",
-};
-
-export default homeText;
